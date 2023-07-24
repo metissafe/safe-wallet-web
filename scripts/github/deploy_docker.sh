35b87c84bef878e46704fca9f18b957c7a6e709d
Moussa Syoufi <b4n4n4s@Moussas-Laptop.local>
2023-07-24 14:01:27 +0300

    added metis-dev docker tag

--- scripts/github/deploy_docker.sh
+++ scripts/github/deploy_docker.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 export DOCKER_BUILDKIT=1
 
-if [ "$1" = "dev" -o "$1" = "main" ]; then
+if [ "$1" = "dev" -o "$1" = "main" -o "$1" = "metis-dev"]; then
     # If image does not exist, don't use cache
     docker pull safeglobal/$DOCKERHUB_PROJECT:$1 && \
     docker build -t $DOCKERHUB_PROJECT . --cache-from safeglobal/$DOCKERHUB_PROJECT:$1 --build-arg BUILDKIT_INLINE_CACHE=1 || \
